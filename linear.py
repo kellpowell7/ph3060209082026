20395342fe00efba57b3643fb65ce6419854a0cd
Keller Powell <kbp0020@uah.edu>
2026-09-06 19:44:16 +0000

    first10_09062026

--- linear.py
+++ linear.py
@@ -11,6 +11,7 @@ from numpy.typing import NDArray
 # Numerical Libraries
 import numpy as np
 from astropy import units as u
+import math
 
 # Local Utilities
 from plotutil import colored_line_between_pts
@@ -109,7 +110,7 @@ def are_commutative(first, second, tolerance=1e-10):
     True | False: boolean
         Returns whether or not the matrices commute.
     """
-    if A @ B == B @ A: return True
+    if first @ second == second @ first: return True
     else: return False
 
 
@@ -175,27 +176,171 @@ def is_linear_operator(matrix, tolerance=1e-10):
         rows, cols = matrix.shape
         if rows == cols: return True
         else: return False
-    else: raise ValueError = ('Matrix is the incorrect size. ')
+    else:raise ValueError('Matrix is the incorrect size. ')
 
 
 def projection(vector_a, vector_b):
-    """Project vector $\\vec{A}$ onto vector $\\vec{B}$."""
+    """Project vector $\\vec{A}$ onto vector $\\vec{B}$.
     
+    Parameters
+    ----------
+    vector_a : np.array
+        A valid numpy vector.
+    vector_b : np.array
+        An additional valid numpy vector.
+    
+    Returns
+    -------
+    proj : np.array
+        The projection vector of vec_a onto vec_b.
+    """
+
+    if np.sum(vector_a) == 0 or np.sum(vector_b) == 0:
+        raise ValueError('One of these vectors is a zero vector. ')
+    else:
+        dot_prod = np.dot(vector_a, vector_b)
+        mag = np.linalg.norm(vector_b)
+        proj = dot_prod / mag**2) * vector_b
+        
+    return proj
 
 def rotate_vector(vector, axis, theta):
-    """Rotate a vector through angle theta about an axis in $\\mathbb{R}^3$."""
+    """Rotate a vector through angle theta about an axis in $\\mathbb{R}^3$.
     
+    Parameters
+    ----------
+    vector : np.array
+        A valid numpy vector.
+    axis : string
+        The axis in R^3 to be used for the transformation.
+    theta : float
+        The angle to be used for the axial rotation.
+    
+    Returns
+    -------
+    np.array
+        The new vector pushed through the decided transformation.
+    """
+    
+    def x_rot(vector, theta):
+        rot_mat = np.array([[1, 0, 0],
+                            [0, np.cos(theta), -np.sin(theta)], 
+                            [0, np.sin(theta), np.cos(theta)]])
+        return rot_mat @ vector
+    
+            
+    def y_rot(vector, theta):
+        rot_mat = np.array([[np.cos(theta), 0, np.sin(theta)], 
+                            [0, 1, 0], 
+                            [-np.sin(theta), 0, np.cos(theta)]])
+        return rot_mat @ vector
+
+
+    def z_rot(vector, theta):
+        rot_mat = np.array([[np.cos(theta), -np.sin(theta), 0], 
+                            [np.sin(theta), np.cos(theta), 0], 
+                            [0, 0, 1]])
+        return rot_mat @ vector
+
+    
+    if axis == '1':
+        return x_rot(vector, theta)
+    elif axis == '2':
+        return y_rot(vector, theta)
+    elif axis == '3':
+        return z_rot(vector, theta)
+    else:
+        raise ValueError('Invalid axis entered. ')
+
 
 def plane_from_points(first, second, third):
-    """Find the plane through three noncollinear points."""
+    """Find the plane through three noncollinear points.
+    
+    Parameters
+    ----------
+    first : np.array
+        The first non-collinear vector.
+    second : np.array
+        The second non-collinear vector.
+    third : np.array
+        The third non-collinear vector.
+    
+    Returns
+    -------
+    vec_n : np.array
+        The normal vector to new plane.
+    std_form : np.array
+        The array of constants from standard form.
+    offset : float
+        The offset of the plane.
+    """
+    
+    vec1 = second - first
+    vec2 = third - first
+    
+    vec_n = np.cross(vec1, vec2)
+    std_form = [vec_n[0], vec_n[1], vec_n[2]]
+    offset = np.dot(vec_n, first)
+    
+    return vec_n, std_form, offset
     
 
 def distance_point_to_plane(point, normal, offset):
-    """Find the minimum distance from a point to a plane."""
+    """Find the minimum distance from a point to a plane.
+    
+    Parameters
+    ----------
+    point : np.array
+        The point that will be used to evaluate the distance.
+    normal : np.array
+        The vector normal to the plane.
+    offset : float
+        The offset from the plane that will be used.
+    
+    Returns
+    -------
+    distance : float
+        The numerical distance between the point and the plane."""
+    
+    numerator = np.abs(np.dot(point, normal) + offset)
+    denominator = np.linalg.norm(normal)
+    
+    distance = numerator / denominator
+    return distance
     
 
 def distance_between_lines(first_point, first_direction, second_point, second_direction):
-    """Find the minimum distance between two lines in $\\mathbb{R}^3$."""
+    """Find the minumum distance between two lines in $\\mathbb{R}^3$
+    
+    Parameters
+    ----------
+    first_point : np.array
+        The beginning point of vector 1.
+    first_direction : np.array
+        The ending point of vector 1.
+    second_point : np.array
+        The beginning point of vector 2.
+    second_direction : np.array
+        The ending point of vector 2.
+    
+    Returns
+    -------
+    d : float
+        The absolute value distance between the lines.
+    """
+    
+    
+    first_point = np.array(first_point, dtype=float)
+    first_direction = np.array(first_direction, dtype=float)
+    second_point = np.array(second_point, dtype=float)
+    second_direction = np.array(second_direction, dtype=float)
+    
+    vec_n = np.cross(first_direction, second_direction)
+    vec_n = vec_n / np.linalg.norm(vec_n)
+        
+    d = np.abs(np.dot(second_point - first_point, vec_n))
+    
+    return d
     
 
 def solve_cable_tension(N, L, rho, g=EARTH_GRAVITY):
